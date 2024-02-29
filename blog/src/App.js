179894d3822873288259f43cbcb338c264f1c436
shjeong99 <mcncshjeong@gmail.com>
2024-02-29 09:26:21 +0900

    레이아웃

---
+++ blog/src/App.js
@@ -0,0 +1,42 @@
+/* eslint-disable */ //warning 없애기
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  let [title, titleFunc] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [cnt, cntFunc] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={ { fontSize : '16px'} }>ReactBlog</h4>
+      </div>
+      <button onClick={ () => {
+         let tempArr = [...title];
+         tempArr.sort();
+         titleFunc(tempArr);
+      } }>가나다순 정렬</button>
+      <div className="list">
+        <h4>{ title[0] } <span onClick={()=>{ cntFunc(cnt++) }}>👍</span> { cnt } </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      {/* <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
