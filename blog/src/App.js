a750ded5fde3ba7d431601aed9a8cb98e329976d
shjeong99 <mcncshjeong@gmail.com>
2024-02-29 17:31:28 +0900

    컴포넌트 만들기

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   let [title, titleFunc] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let [cnt, cntFunc] = useState(0);
+  let [modalYN, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -17,7 +18,7 @@ function App() {
          tempArr.sort();
          titleFunc(tempArr);
       } }>가나다순 정렬</button>
-      <div className="list">
+      {/* <div className="list">
         <h4>{ title[0] } <span onClick={()=>{ cntFunc(cnt++) }}>👍</span> { cnt } </h4>
         <p>2월 17일 발행</p>
       </div>
@@ -26,15 +27,35 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ title[2] }</h4>
-        <p>2월 17일 발행</p>
-      </div>
+        <h4 onClick={() => {
+            setModal(!modalYN); 
+          }}>{ title[2] }</h4>
+        <Date/>
+      </div> */}
+      {
+        title.map(function(a , i){
+          return (
+            <div className="list">
+              <h4>{ title[i] }</h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+      {
+        modalYN == true ? <Modal/> : null
+      }
 
-      {/* <div className='modal'>
+    </div>
+  );
+}
+
+const Modal = () => {
+  return (
+    <div className="modal">
         <h4>제목</h4>
         <p>날짜</p>
         <p>상세내용</p>
-      </div> */}
     </div>
   );
 }
