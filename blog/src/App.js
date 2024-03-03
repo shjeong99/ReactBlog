8789747e2a52bb814ba10dcf6e7e3a67f4a4ae5e
DESKTOP-IO9TGIJ\정서형 <shjeong99>
2024-03-03 20:23:17 +0900

    props로 값 전달하기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,31 @@
 /* eslint-disable */ //warning 없애기
 
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let [title, titleFunc] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [cnt, cntFunc] = useState(0);
+  let [title, titleFunc] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬독학",
+  ]);
+  let [cnt, cntFunc] = useState([0, 0, 0]);
   let [modalYN, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ { fontSize : '16px'} }>ReactBlog</h4>
+        <h4 style={{ fontSize: "16px" }}>ReactBlog</h4>
       </div>
-      <button onClick={ () => {
-         let tempArr = [...title];
-         tempArr.sort();
-         titleFunc(tempArr);
-      } }>가나다순 정렬</button>
+      <button
+        onClick={() => {
+          let tempArr = [...title];
+          tempArr.sort();
+          titleFunc(tempArr);
+        }}
+      >
+        가나다순 정렬
+      </button>
       {/* <div className="list">
         <h4>{ title[0] } <span onClick={()=>{ cntFunc(cnt++) }}>👍</span> { cnt } </h4>
         <p>2월 17일 발행</p>
@@ -32,32 +40,53 @@ function App() {
           }}>{ title[2] }</h4>
         <Date/>
       </div> */}
-      {
-        title.map(function(a , i){
-          return (
-            <div className="list">
-              <h4>{ title[i] }</h4>
-              <p>2월 17일 발행</p>
-            </div>
-          )
-        })
-      }
-      {
-        modalYN == true ? <Modal/> : null
-      }
-
+      {title.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setModal(!modalYN);
+              }}
+            >
+              {title[i]}
+              <span
+                onClick={() => {
+                  let tempCnt = [...cnt];
+                  console.log(tempCnt);
+                  tempCnt[i]++;
+                  cntFunc(tempCnt);
+                }}
+              >
+                👍
+              </span>
+              {cnt[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+      {modalYN == true ? <Modal title={title} titleFunc={titleFunc} /> : null}
     </div>
   );
 }
 
-const Modal = () => {
+const Modal = (props) => {
   return (
     <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
+      <h4>{props.title[0]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button
+        onClick={() => {
+          let tempProp = [...props.title];
+          tempProp[0] = "여자 코트 추천";
+          props.titleFunc(tempProp);
+        }}
+      >
+        글수정
+      </button>
     </div>
   );
-}
+};
 
 export default App;
